8133721f537164d1a04843100737b69c250ac243
Opilane TTHK <opilane@tthk.ee>
2021-09-08 12:30:53 +0300

    add random

---
+++ EpicDiceGame/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EpicDiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus osaleb kaks mängijat
+            //iga mängija viskab täringut 1-6
+            //programm kuvab nende visete tulemused ja kuvab ka kes on võitja
+            //ehk kes viskas suuremat
+
+            Random rnd = new Random();
+
+            int MyRandomNumber = rnd.Next(1, 7);
+
+            int MyRandomNumber = rnd.Next(1, 7);
+        }
+    }
+}
---
+++ RandomNumberGenerator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace RandomNumberGenerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int MyRandomNumber = rnd.Next(1, 11);
+
+
+
+            //programm genereerib juhuslikku numbrit 1-10
+            //programm kontrollib kas genereeritud number on
+            //suurem kui 5
+            //kui number on suurem kui 5 siis konsool kuvab
+            //"juhuslik number on(myrandomnumber), see on suurem
+            //kui 5"
+            //kui number on väiksm kui 5 siis konsool kuvab
+            //juhuslik number on(myrandomnumber), see on väiksem
+            //kui 5
+            //kui number on 5, siis konsool kuvab "juhslik number on 5"
+
+
+            Console.WriteLine(MyRandomNumber);
+            if (MyRandomNumber > 5)
+            {
+                Console.WriteLine($"juhuslik Number on {MyRandomNumber} see on suurm, kui 5");
+            }
+            else if (MyRandomNumber < 5)
+            {
+                Console.WriteLine($"Juhuslik number on {MyRandomNumber} see on väiksem kui 5");
+            }
+            else
+            {
+             Console.WriteLine($"Juhuslik number ongi 5");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
